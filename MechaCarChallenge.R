87613fba4c5ea379608c537698c5c5241b7d34a4
cloestreich <108380062+cloestreich@users.noreply.github.com>
2022-10-08 15:24:53 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,10 @@
+#Deliverable 1
 library(dplyr)
 Mecha_data <- read.csv('MechaCar_mpg.csv') #import dataset
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_data)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_data))
+
+#Deliverable 2
+suspension_data <- read.csv('Suspension_Coil.csv') #import dataset
+total_summary <- suspension_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
